788493f8e501d4642755dc329bfb6c22e3582d95
Ashfaq Farleen <139785309+devmaniac1@users.noreply.github.com>
2024-07-15 17:02:29 +0000

    UseContext custom hook as useCities

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const BASE_URL = "https://lglxpq-8000.csb.app";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const cities = await res.json();
+        setCities(cities);
+      } catch (e) {
+        console.log("Error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const city = await res.json();
+      setCurrentCity(city);
+    } catch (e) {
+      console.log("Error");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside of the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
