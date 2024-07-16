24370b0b912477b9c864505d2f0f30752637c1d3
Mohammed Ashfaq <mohammed.20221656@iit.ac.lk>
2024-07-16 16:34:46 +0000

    Final UseContext and ProtectedRoute

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,12 @@
 import styles from "./AppLayout.module.css";
-import AppNav from "../components/AppNav";
 import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
+import User from "../components/User";
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
+      <User />
       <Sidebar />
       <Map />
     </div>
