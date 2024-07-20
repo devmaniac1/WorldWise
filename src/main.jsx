b4dda5289d6845d95685cea8525f60854912cc5c
Mohammed Ashfaq <mohammed.20221656@iit.ac.lk>
2024-07-20 12:51:20 +0000

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SpeedInsights>
+    <>
       <App />
-    </SpeedInsights>
+      <SpeedInsights />
+    </>
   </React.StrictMode>,
 );
